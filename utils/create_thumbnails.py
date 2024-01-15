164d3581267864d5610428d1fde51f9f7ca7fb7e
baniceBOA <31449597+baniceBOA@users.noreply.github.com>
2024-01-15 22:34:25 +0300

    Change the thumn file

--- utils/create_thumbnails.py
+++ utils/create_thumbnails.py
@@ -1,4 +1,4 @@
-import cv2
+
 import os
 
 
@@ -8,20 +8,54 @@ def create_thumbnail(filename, output_dir=None):
 
     '''
     thumbname = f'{os.path.splitext(filename.split(os.sep)[-1])[0]}.png'
-    vcap = cv2.VideoCapture(filename)
-    res, im_ar = vcap.read()
-    
-    if res:
-        while im_ar.mean() < 10 and res:
-            res, im_ar = vcap.read()
-        im_ar = cv2.resize(im_ar, (400, 400), 0, 0, cv2.INTER_LINEAR)
-        #to save we have two options
-        #1) save on a file
-        if output_dir:
-            cv2.imwrite(os.path.join(output_dir, thumbname), im_ar)
-            return os.path.join(output_dir, thumbname)
+    try:
+        import cv as cv2
+        vcap = cv2.VideoCapture(filename)
+        res, im_ar = vcap.read()
+        
+        if res:
+            while im_ar.mean() < 10 and res:
+                res, im_ar = vcap.read()
+            im_ar = cv2.resize(im_ar, (400, 400), 0, 0, cv2.INTER_LINEAR)
+            #to save we have two options
+            #1) save on a file
+            if output_dir:
+                cv2.imwrite(os.path.join(output_dir, thumbname), im_ar)
+                return os.path.join(output_dir, thumbname)
+            else:
+                cv2.imwrite(thumbname, im_ar)
+                return thumbname
         else:
-            cv2.imwrite(thumbname, im_ar)
-            return thumbname
-    else:
-        return None
+            return None
+    except Exception as e:
+        print(f'Failed with error:{e}')
+        try:
+            from moviepy.editor import VideoFileClip
+            from PIL import Image
+            clips = VideoFileClip(filename)
+            frames = clips.reader.fps
+            max_duration = int(clips.duration)+1
+            mid = max_duration//2
+
+            frame = clips.get_frame(mid)
+            thumbnail = Image.fromarray(frame)
+            if output_dir:
+                thumbnail.save(os.path.join(output_dir, thumbname))
+                return os.path.join(output_dir, thumbname)
+            else:
+                thumbnail.save(thumbname)
+                return thumbname
+        except Exception as e:
+            print('Failed to create a thumbname using default')
+            from PIL import Image
+            defaultimage = Image.open('/assests/thumbs/videothumb.png')
+            if output_dir:
+                defaultimage.save(os.path.join(output_dir, thumbname))
+                return os.path.join(output_dir, thumbname)
+            else:
+                defaultimage.save(thumbname)
+                return thumbname
+
+
+
+
