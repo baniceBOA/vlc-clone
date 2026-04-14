39a91dffac028da520b79d6210cc56b23104d32f
baniceBOA <31449597+baniceBOA@users.noreply.github.com>
2026-04-15 00:36:39 +0300

    Rest Refactor 2

--- tmp_mdbottomsheet_inspect.py
+++
@@ -1,9 +0,0 @@
-from kivymd.uix.bottomsheet import MDBottomSheet
-import inspect, pathlib
-print('methods')
-print([m for m in dir(MDBottomSheet) if not m.startswith('_')])
-print('file', inspect.getsourcefile(MDBottomSheet))
-with open(inspect.getsourcefile(MDBottomSheet), 'r', encoding='utf-8') as f:
-    text = f.read()
-idx = text.find('class MDBottomSheet')
-print(text[idx:idx+1200])
--- tmp_mdbottomsheet_source.py
+++
@@ -1,8 +0,0 @@
-import inspect, pathlib
-from kivymd.uix.bottomsheet import MDBottomSheet
-path = pathlib.Path(inspect.getsourcefile(MDBottomSheet))
-text = path.read_text()
-idx = text.find('class MDBottomSheet')
-print('file:', path)
-print('idx', idx)
-print(text[idx:idx+4000])
